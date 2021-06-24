8c859b354b82537f747d3b70764a734d44dd9694
mayachos <amacrk@icloud.com>
2021-06-24 13:00:14 +0900

    change main design

--- techneko.js
+++ techneko.js
@@ -2,17 +2,31 @@
 function fadeAnime() {
 
     //ふわっと動くきっかけのクラス名と動きのクラス名の設定
-    $('.fadeUpTrigger').each(function() { //fadeInUpTriggerというクラス名が
-        　　 var elemPos = $(this).offset().top - 5; //要素より、50px上の
-        　　 var scroll = $(window).scrollTop();　　 var windowHeight = $(window).height();　　 if (scroll >= elemPos - windowHeight) {　　 $(this).addClass('fadeUp');　　 // 画面内に入ったらfadeInというクラス名を追記
-            　　 } else {　　　 $(this).removeClass('fadeUp');　　 // 画面外に出たらfadeInというクラス名を外す
-            　　 }　　 });
-    $('.fadeInTrigger').each(function() { //fadeInUpTriggerというクラス名が
-        　　 var elemPos = $(this).offset().top - 50; //要素より、50px上の
-        　　 var scroll = $(window).scrollTop();　　 var windowHeight = $(window).height();　　 if (scroll >= elemPos - windowHeight) {　　 $(this).addClass('fadeIn');　　 // 画面内に入ったらfadeInというクラス名を追記
-            　　 } else {　　　 $(this).removeClass('fadeIn');　　 // 画面外に出たらfadeInというクラス名を外す
-            　　 }　　 });
-    $('.blurTrigger').each(function() { //blurTriggerというクラス名が
+    $('.fadeUpTrigger').each(function () { //fadeInUpTriggerというクラス名が
+        var elemPos = $(this).offset().top - 5; //要素より、50px上の
+        var scroll = $(window).scrollTop(); var windowHeight = $(window).height(); if (scroll >= elemPos - windowHeight) {
+            $(this).addClass('fadeUp');　　 // 画面内に入ったらfadeInというクラス名を追記
+        } else {
+            $(this).removeClass('fadeUp');　　 // 画面外に出たらfadeInというクラス名を外す
+        }
+    });
+    $('.fadeRightTrigger').each(function () { //fadeInUpTriggerというクラス名が
+        var elemPos = $(this).offset().top - 5; //要素より、50px上の
+        var scroll = $(window).scrollTop(); var windowHeight = $(window).height(); if (scroll >= elemPos - windowHeight) {
+            $(this).addClass('fadeRight');　　 // 画面内に入ったらfadeInというクラス名を追記
+        } else {
+            $(this).removeClass('fadeRight');　　 // 画面外に出たらfadeInというクラス名を外す
+        }
+    });
+    $('.fadeInTrigger').each(function () { //fadeInUpTriggerというクラス名が
+        var elemPos = $(this).offset().top - 50; //要素より、50px上の
+        var scroll = $(window).scrollTop(); var windowHeight = $(window).height(); if (scroll >= elemPos - windowHeight) {
+            $(this).addClass('fadeIn');　　 // 画面内に入ったらfadeInというクラス名を追記
+        } else {
+            $(this).removeClass('fadeIn');　　 // 画面外に出たらfadeInというクラス名を外す
+        }
+    });
+    $('.blurTrigger').each(function () { //blurTriggerというクラス名が
         var elemPos = $(this).offset().top - 55; //要素より、50px上の
         var scroll = $(window).scrollTop();
         var windowHeight = $(window).height();
@@ -27,12 +41,12 @@ function fadeAnime() {
 } //ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
 
 // 画面をスクロールをしたら動かしたい場合の記述
-$(window).scroll(function() {
+$(window).scroll(function () {
     fadeAnime(); /* アニメーション用の関数を呼ぶ*/
 }); // ここまで画面をスクロールをしたら動かしたい場合の記述
 
 // 画面が読み込まれたらすぐに動かしたい場合の記述
-$(window).on('load', function() {
+$(window).on('load', function () {
     fadeAnime(); /* アニメーション用の関数を呼ぶ*/
 }); // ここまで画面が読み込まれたらすぐに動かしたい場合の記述
 
@@ -44,6 +58,6 @@ stalker.id = 'stalker'; //IDを付与
 document.body.appendChild(stalker); //bodyの最後に挿入
 
 //上記のdivタグをマウスに追従させる処理
-document.addEventListener('mousemove', function(e) {
+document.addEventListener('mousemove', function (e) {
     stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
 });
